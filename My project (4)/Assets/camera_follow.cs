f284daee13215b683d4606813a836a7b04f63b6b
Boban005 <bogibossss@gmail.com>
2023-07-05 18:56:44 +0200

    Added camera_follow.

---
+++ My project (4)/Assets/camera_follow.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using TreeEditor;
+using UnityEngine;
+
+public class camerafollow : MonoBehaviour
+{
+    public Transform target;
+    private Vector3 velocity;
+    public float smooth;
+    public Vector3 cameraOffset;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = Vector3.SmoothDamp(transform.position, target.position + cameraOffset, ref velocity, smooth);
+
+    }
+}
