342209c8374cb7637d3f3ce4f2014c45c8df2239
Boban005 <bogibossss@gmail.com>
2023-07-07 15:53:52 +0200

    ne znam 2

--- My project (4)/Assets/Scripts/PlayerMovement.cs
+++ My project (4)/Assets/Scripts/PlayerMovement.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
 	static public float rotationAngle;
+	static public float deltaX, deltaY;
 	
 	Vector2 orientation;
 	Rigidbody2D rb;
@@ -15,8 +16,8 @@ public class PlayerMovement : MonoBehaviour
 	public Vector3 bulletSpawnerOffset;
 	public GameObject camera;
 	public float delayFactor;
-	float deltaX, deltaY;
 	public Animator animator;
+	bool right;
 
 	void Start()
 	{
@@ -24,32 +25,61 @@ public class PlayerMovement : MonoBehaviour
 		transform.position = Vector3.zero;
 		gunOffset = new Vector3(0.1f, 0.1f, -2f);
 		bulletSpawnerOffset = new Vector3(0.6f, 0.09f, -4f);
+		right = true;
 	}
 
 	void Update()
 	{
-		float moveX = Input.GetAxis("Horizontal");
-		float moveY = Input.GetAxis("Vertical");
+		float moveX = Input.GetAxisRaw("Horizontal");
+		float moveY = Input.GetAxisRaw("Vertical");
 		orientation = new Vector2(moveX, moveY).normalized;
         animator.SetBool("isMoving", moveX != 0 || moveY != 0);
+		
+		if (moveX < 0) right = false;
+		if (moveX > 0) right = true;
+
+		if(right == false)
+		{
+			transform.rotation = Quaternion.Euler(new Vector3(0.0f, 180.0f, 0.0f));
+		}
+		else
+		{
+			transform.rotation = Quaternion.Euler(new Vector3(0.0f, 0.0f, 0.0f));
+		}
 
         gun.transform.position = transform.position + gunOffset;
 		if (deltaX < 0)
 		{
+
 			float finalX = bulletSpawnerOffset.x * Mathf.Cos(rotationAngle) + bulletSpawnerOffset.y * Mathf.Sin(rotationAngle);
 			float finalY = bulletSpawnerOffset.x * Mathf.Sin(rotationAngle) - bulletSpawnerOffset.y * Mathf.Cos(rotationAngle);
 			bulletSpawner.transform.position = gun.transform.position + new Vector3(-finalX, finalY, bulletSpawnerOffset.z);
 		}
 		else
 		{
+
 			float finalX = bulletSpawnerOffset.x * Mathf.Cos(rotationAngle) + bulletSpawnerOffset.y * Mathf.Sin(rotationAngle);
 			float finalY = bulletSpawnerOffset.x * Mathf.Sin(rotationAngle) - bulletSpawnerOffset.y * Mathf.Cos(rotationAngle);
 			bulletSpawner.transform.position = gun.transform.position + new Vector3(finalX, finalY, bulletSpawnerOffset.z);
 		}
 
-		
-		
-	}
+        rb.velocity = new Vector2(orientation.x * moveSpeed, orientation.y * moveSpeed);
+
+        deltaY = Input.mousePosition.y - transform.position.y - Screen.height / 2;
+        deltaX = Input.mousePosition.x - transform.position.x - Screen.width / 2;
+        if (deltaX < 0)
+        {
+            rotationAngle = Mathf.Atan2(deltaY, -deltaX);
+            gun.transform.rotation = Quaternion.Euler(new Vector3(0.0f, 180.0f, rotationAngle * 180 / Mathf.PI));
+        }
+        else
+        {
+            rotationAngle = Mathf.Atan2(deltaY, deltaX);
+            gun.transform.rotation = Quaternion.Euler(new Vector3(0.0f, 0.0f, rotationAngle * 180 / Mathf.PI));
+        }
+        gunOffset = new Vector3(deltaX / 100 * delayFactor, gunOffset.y, gunOffset.z);
+
+    }
 
 	private void FixedUpdate()
 	{
